b2871aa8d80ee0b3016eb12dd785fe126c4d690d
IBC Direk Jongpum <Direk@sritranggroup.com>
2026-08-31 09:10:30 +0700

    feat: change log to warn

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const BASE_URL = process.env.BASE_URL;
 
 // Middleware ดักแยกระหว่าง LINE กับ Postman/Browser
 const conditionalMiddleware = (req, res, next) => {
-  console.log("x-01-m");
+  console.warn("x-01-m");
   if (req.headers['x-line-signature']) {
     return line.middleware(config)(req, res, next);
   }
@@ -23,13 +23,13 @@ const conditionalMiddleware = (req, res, next) => {
 
 app.post('/api', conditionalMiddleware, async (req, res) => {
   try {
-    console.log("x-01");
+    console.warn("x-01");
     const events = req.body?.events;
-    console.log("x-02");
+    console.warn("x-02");
     if (!Array.isArray(events)) {
       return res.status(200).json({ code: 200, result: "success", message: "No events or non-array" });
     }
-    console.log("x-03");
+    console.warn("x-03");
     for (const event of events) {
       const { type, source, message } = event;
       const userId = source?.userId;
@@ -38,32 +38,32 @@ app.post('/api', conditionalMiddleware, async (req, res) => {
 
       if (type === 'message' && message?.type === 'text') {
         const text = message.text ? message.text.trim() : '';
-        console.log("x-04");
+        console.warn("x-04");
         if (text.toLowerCase().startsWith('gid')) {
           if (!BASE_URL) {
             console.error('BASE_URL is not defined in Environment Variables');
           } else {
             try {
               // ตัด slash ต่อท้าย BASE_URL ป้องกัน URL ซ้ำซ้อน (เช่น //gwcenter)
-              console.log("x-05");
-              console.log(userId);
-              console.log(text);
-              console.log("x-05-2");
+              console.warn("x-05");
+              console.warn(userId);
+              console.warn(text);
+              console.warn("x-05-2");
               // const cleanBaseUrl = BASE_URL.replace(/\/+$/, '');
               await axios.post(`https://203.151.152.127/gwcenter/api/v1/servicelineoa/matchuserline/`, {
                 userId: userId,
                 message: text
               }, { timeout: 5000 });
-              console.log("x-06");
+              console.warn("x-06");
             } catch (apiErr) {
-              console.log("x-07");
+              console.warn("x-07");
               console.error('Call External API Error:', apiErr.response?.data || apiErr.message);
             }
           }
         }
       }
     }
-    console.log("x-08");
+    console.warn("x-08");
     return res.status(200).json({ code: 200, result: "success", message: "Processed successfully", data: 0 });
   } catch (error) {
     console.error('Webhook Unhandled Error:', error.message);
